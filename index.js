1dc858b511befe5aaa7b0cbbb99c80fa16ca3286
David Caldera <davidcaldera13@yahoo.com>
2022-02-25 11:47:45 -0600

    commit tree d65d9a892543329b3795468eeb9ce2d079bbe9b3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const writeCards = (name, event) => {
+    let copy = [...name]
+    let copy2= copy
+    for (let i=0; i < copy2.length; i++) {
+        copy2.splice([i], 1, `Thank you, ${copy[i]}, for the wonderful ${event} gift!`);
+    }
+    return copy2
+}
+
+function countDown(num) {
+    let i = 0
+    while (i < num +1) {
+        console.log(i)
+        i++
+    }
+}
